c92e0f894e37426edc4d94d6868d96daf82125f6
David Martínez <gollumdeagol@gmail.com>
2015-04-07 12:29:56 +0200

    added MOA report to .txt dumper script

---
+++ report-dumper.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+import argparse
+import os
+from subprocess import call
+
+
+def executeMOATask(reportFile, outDir):
+	basename = os.path.splitext(os.path.basename(reportFile.name))[0]
+	outFile = outDir + '/' + basename + '.txt'
+	cmdFormat = {
+		'report': reportFile.name,
+		'out': outFile
+	}
+	cmd = './moa.sh "ReadAnonymizationReport -r %(report)s" > %(out)s' % cmdFormat
+	print cmd
+	#call(cmd, shell=True)
+
+def readReportsWithArgs(args):
+	for file in args.report_files:
+		executeMOATask(file, args.out_dir)
+
+if __name__ == '__main__':
+	# argument parsing
+	parser = argparse.ArgumentParser(
+						description='Executes MOA to read anonymization reports and dump them in the'+
+									' specified directory.')
+	parser.add_argument('-r', '--report-files', nargs='+', type=argparse.FileType('r'), required=True,
+						help='the MOA report files to be read')
+	parser.add_argument('-o', '--out-dir', required=True,
+						help='the directory where the dumped report will be written')
+	args = parser.parse_args()
+
+	# main procedure
+	readReportsWithArgs(args)
